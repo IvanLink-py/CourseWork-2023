722491ccbbfbb24007faf84e21a0dbf9f74c9ee4
IvanLink-py <ostaninivan8@gmail.com>
2022-11-10 01:16:39 +0500

    Рефакторинг

--- drawSQL-mysql-export.sql
+++ drawSQL-mysql-export.sql
@@ -1,5 +1,11 @@
+DROP DATABASE IF EXISTS NewsFactory;
+CREATE DATABASE NewsFactory ;
+USE NewsFactory;
+
+--@block
+DROP TABLE IF EXISTS `Voice Model`;
 CREATE TABLE `Voice Model`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `SecondPerSymbol` DOUBLE NOT NULL,
     `SecondPerWord` DOUBLE NOT NULL,
     `SecondPerComa` DOUBLE NOT NULL,
@@ -8,13 +14,11 @@ CREATE TABLE `Voice Model`(
     `SecondPerHyphen` DOUBLE NOT NULL
 );
 
-ALTER TABLE
-    `Voice Model`
-ADD
-    PRIMARY KEY `voice model_id_primary`(`id`);
 
+--@block
+DROP TABLE IF EXISTS `Person`;
 CREATE TABLE `Person`(
-    `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
     `FirstName` VARCHAR(255) NOT NULL,
     `SecondName` VARCHAR(255) NOT NULL,
     `LastName` VARCHAR(255) NULL,
@@ -22,228 +26,163 @@ CREATE TABLE `Person`(
     `VoiceModel` INT NULL
 );
 
-ALTER TABLE
-    `Person`
-ADD
-    PRIMARY KEY `person_id_primary`(`ID`);
+--@block
+DROP TABLE IF EXISTS `Post`;
+CREATE TABLE `Post`(`Name` VARCHAR(255) PRIMARY KEY);
 
-CREATE TABLE `Post`(`Name` VARCHAR(255) NOT NULL);
-
-ALTER TABLE
-    `Post`
-ADD
-    PRIMARY KEY `post_name_primary`(`Name`);
-
+--@block
+DROP TABLE IF EXISTS `Script`;
 CREATE TABLE `Script`(
-    `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
     `Duration` TIME NOT NULL,
     `Author` INT NOT NULL
 );
 
-ALTER TABLE
-    `Script`
-ADD
-    PRIMARY KEY `script_id_primary`(`ID`);
-
+--@block
+DROP TABLE IF EXISTS `Project`;
 CREATE TABLE `Project`(
-    `ID` INT NOT NULL,
+    `ID` INT PRIMARY KEY,
     `Name` VARCHAR(255) NOT NULL,
     `Description` TEXT NOT NULL,
     `Script` INT NOT NULL,
-    `Type` ENUM('') NOT NULL,
+    `Type` ENUM('efir', 'report', 'advertising', 'film', 'other') NOT NULL,
     `Path` TEXT NOT NULL
 );
 
-ALTER TABLE
-    `Project`
-ADD
-    PRIMARY KEY `project_id_primary`(`ID`);
-
+--@block
+DROP TABLE IF EXISTS `Material`;
 CREATE TABLE `Material`(
-    `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
     `Filming` INT NULL,
     `Name` VARCHAR(255) NOT NULL,
     `Description` TEXT NULL,
     `Path` TEXT NOT NULL,
     `Duration` TIME NULL,
-    `Type` ENUM('') NOT NULL,
+    `Type` ENUM('pictures', 'interview', 'combined') NULL,
     `Size` DOUBLE NOT NULL,
     `Camera` INT NULL
 );
 
-ALTER TABLE
-    `Material`
-ADD
-    PRIMARY KEY `material_id_primary`(`ID`);
 
+--@block
+DROP TABLE IF EXISTS `Project Import`;
 CREATE TABLE `Project Import`(
-    `Project_ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `Material_ID` INT NOT NULL
+    `Project_ID` INT NOT NULL,
+    `Material_ID` INT NOT NULL,
+    PRIMARY KEY (`Project_ID`, `Material_ID`)
 );
 
-ALTER TABLE
-    `Project Import`
-ADD
-    PRIMARY KEY `project import_project_id_primary`(`Project_ID`);
 
-ALTER TABLE
-    `Project Import`
-ADD
-    PRIMARY KEY `project import_material_id_primary`(`Material_ID`);
-
+--@block
+DROP TABLE IF EXISTS `Filming`;
 CREATE TABLE `Filming`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `Name` VARCHAR(255) NOT NULL,
     `Description` TEXT NULL,
     `Address` TEXT NULL,
     `Time` DATETIME NULL,
-    `Status` ENUM('') NOT NULL
+    `Status` ENUM('planned', 'canceled', 'in work', 'finished', 'other') NOT NULL
 );
 
-ALTER TABLE
-    `Filming`
-ADD
-    PRIMARY KEY `filming_id_primary`(`id`);
 
+--@block
+DROP TABLE IF EXISTS `Tag`;
 CREATE TABLE `Tag`(
-    `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `ID` INT PRIMARY KEY AUTO_INCREMENT,
     `Name` VARCHAR(255) NOT NULL,
     `Parrent` INT NULL
 );
 
-ALTER TABLE
-    `Tag`
-ADD
-    PRIMARY KEY `tag_id_primary`(`ID`);
 
+--@block
+DROP TABLE IF EXISTS `Material tag`;
 CREATE TABLE `Material tag`(
-    `Material` INT UNSIGNED NOT NULL,
-    `tag` INT UNSIGNED NOT NULL
+    `Material` INT NOT NULL,
+    `tag` INT NOT NULL,
+    PRIMARY KEY(`Material`, `tag`)
 );
 
-ALTER TABLE
-    `Material tag`
-ADD
-    PRIMARY KEY `material tag_material_primary`(`Material`);
 
-ALTER TABLE
-    `Material tag`
-ADD
-    PRIMARY KEY `material tag_tag_primary`(`tag`);
-
+--@block
+DROP TABLE IF EXISTS `Filming tag`;
 CREATE TABLE `Filming tag`(
-    `Filming` INT UNSIGNED NOT NULL,
-    `tag` INT NOT NULL
+    `Filming` INT NOT NULL,
+    `tag` INT NOT NULL,
+    PRIMARY KEY(`Filming`, `tag`)
 );
 
-ALTER TABLE
-    `Filming tag`
-ADD
-    PRIMARY KEY `filming tag_filming_primary`(`Filming`);
 
-ALTER TABLE
-    `Filming tag`
-ADD
-    PRIMARY KEY `filming tag_tag_primary`(`tag`);
-
+--@block
+DROP TABLE IF EXISTS `Project tag`;
 CREATE TABLE `Project tag`(
-    `Project` INT UNSIGNED NOT NULL,
-    `tag` INT UNSIGNED NOT NULL
+    `Project` INT NOT NULL,
+    `tag` INT NOT NULL,
+    PRIMARY KEY(`Project`, `tag`)
 );
 
-ALTER TABLE
-    `Project tag`
-ADD
-    PRIMARY KEY `project tag_project_primary`(`Project`);
-
-ALTER TABLE
-    `Project tag`
-ADD
-    PRIMARY KEY `project tag_tag_primary`(`tag`);
-
+--@block
+DROP TABLE IF EXISTS `Filming crew`;
 CREATE TABLE `Filming crew`(
     `Filming` INT NOT NULL,
     `Person` INT NOT NULL,
-    `Role` ENUM('') NOT NULL
+    `Role` ENUM('operator', 'reporter', 'driver', 'other') NOT NULL,
+    PRIMARY KEY(`Filming`, `Person`, `Role`)
 );
 
-ALTER TABLE
-    `Filming crew`
-ADD
-    PRIMARY KEY `filming crew_filming_primary`(`Filming`);
 
-ALTER TABLE
-    `Filming crew`
-ADD
-    PRIMARY KEY `filming crew_person_primary`(`Person`);
-
-ALTER TABLE
-    `Filming crew`
-ADD
-    PRIMARY KEY `filming crew_role_primary`(`Role`);
-
+--@block
+DROP TABLE IF EXISTS `Camera`;
 CREATE TABLE `Camera`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `Name` VARCHAR(255) NOT NULL,
     `Model` VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE
-    `Camera`
-ADD
-    PRIMARY KEY `camera_id_primary`(`id`);
-
+--@block
+DROP TABLE IF EXISTS `Project crew`;
 CREATE TABLE `Project crew`(
-    `Project` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `Person` INT UNSIGNED NOT NULL,
-    `Role` ENUM('') NOT NULL
+    `Project` INT NOT NULL AUTO_INCREMENT,
+    `Person` INT NOT NULL,
+    `Role` ENUM('author', 'editor', 'video engineer', 'other') NOT NULL,
+    PRIMARY KEY(`Project`, `Person`, `Role`)
 );
-
-ALTER TABLE
-    `Project crew`
-ADD
-    PRIMARY KEY `project crew_project_primary`(`Project`);
-
-ALTER TABLE
-    `Project crew`
-ADD
-    PRIMARY KEY `project crew_person_primary`(`Person`);
-
-ALTER TABLE
-    `Project crew`
-ADD
-    PRIMARY KEY `project crew_role_primary`(`Role`);
-
+--@block
 ALTER TABLE
     `Person`
 ADD
     CONSTRAINT `person_voicemodel_foreign` FOREIGN KEY(`VoiceModel`) REFERENCES `Voice Model`(`id`);
 
+--@block
 ALTER TABLE
     `Person`
 ADD
     CONSTRAINT `person_post_foreign` FOREIGN KEY(`Post`) REFERENCES `Post`(`Name`);
 
+--@block
 ALTER TABLE
     `Project`
 ADD
     CONSTRAINT `project_script_foreign` FOREIGN KEY(`Script`) REFERENCES `Script`(`ID`);
 
+--@block
 ALTER TABLE
     `Script`
 ADD
     CONSTRAINT `script_author_foreign` FOREIGN KEY(`Author`) REFERENCES `Person`(`ID`);
 
+--@block
 ALTER TABLE
     `Tag`
 ADD
     CONSTRAINT `tag_parrent_foreign` FOREIGN KEY(`Parrent`) REFERENCES `Tag`(`ID`);
 
+--@block
 ALTER TABLE
     `Material`
 ADD
     CONSTRAINT `material_filming_foreign` FOREIGN KEY(`Filming`) REFERENCES `Filming`(`id`);
 
+--@block
 ALTER TABLE
     `Material`
 ADD
